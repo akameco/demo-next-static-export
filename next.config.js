9680a4c18424af530299b788dd071ed4e4ad31df
akameco <akameco.t@gmail.com>
2017-07-29 17:27:28 +0900

    Fix asset path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 // @flow
+const dev = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   exportPathMap() {
     return { '/': { page: '/' } }
   },
+  assetPrefix: dev
+    ? 'localhost:3000'
+    : 'https://akameco.github.io/demo-next-static-export/',
 }
