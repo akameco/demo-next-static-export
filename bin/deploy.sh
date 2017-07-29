84cb8a58460a1fc03ae75b948f7590b1888bdf0c
akameco <akameco.t@gmail.com>
2017-07-29 19:21:46 +0900

    Update deploy script
    https://github.com/zeit/next.js/wiki/Deploying-a-Next.js-app-into-GitHub-Pages

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,6 @@ set -e
 git config user.email $GIT_EMAIL
 git config user.name $GIT_NAME
 
+touch out/.nojekyll
+
 npm run gh-pages -- -r https://$GH_TOKEN@github.com/akameco/demo-next-static-export.git
