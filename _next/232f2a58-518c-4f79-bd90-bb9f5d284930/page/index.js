ce4667dbbf2e62d3eedfccc20894091a02b277bd
Travis CI User <travis@example.org>
2017-07-30 04:27:20 +0000

    Updates

--- _next/232f2a58-518c-4f79-bd90-bb9f5d284930/page/index.js
+++ _next/232f2a58-518c-4f79-bd90-bb9f5d284930/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[345]);
+            var comp = module.exports=webpackJsonp([2],[],[435]);
             return { page: comp.default }
           })
         
